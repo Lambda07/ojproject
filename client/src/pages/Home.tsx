03c12e03d4c06d18f019fec95820f60b615096f5
Lambda07 <155990728+Lambda07@users.noreply.github.com>
2024-06-01 19:13:18 +0530

    Add files via upload

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import ProblemList from "../components/ProblemList";
+import { asyncProblemGet } from "../store/ProblemSlice";
+import { RootState } from "../store/store";
+
+export default function Home() {
+  const dispatch = useDispatch()
+  const problems = useSelector((state: RootState) => state.problem.problems)
+
+  useEffect(() => {
+    dispatch(asyncProblemGet() as any)
+  }, [])
+
+  return (
+    <>
+      <div className="max-w-4xl mx-auto mb-8">
+        <div>
+          <img src="./bg23.png" alt="" className="mx-auto" />
+        </div>
+        <div className="font-mono mt-8">
+          <h1 className="text-3xl mb-8">Select a problem to solve</h1>
+          <div>
+            <ProblemList />
+          </div>
+        </div>
+      </div>
+      <footer className="fixed bottom-0 left-0">
+    
+      </footer>
+    </>
+  );
+}
